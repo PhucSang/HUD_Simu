8750017722571dbd46b8fa5513efd94b826ba9b9
PhucSang <nguyenphucsang5@gmail.com>
2026-04-23 15:57:30 +0700

    c

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // Import cấu hình cơ bản từ SillyTavern core
 import { extension_settings } from "../../../extensions.js";
+import { saveSettingsDebounced } from "../../../../script.js";
 
 // Tên này PHẢI khớp chính xác với tên thư mục chứa tiện ích của bạn
 const extensionName = "HUD_Simu"; 
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+
+const defaultSettings = {
+    enabled: false
+};
+
+async function loadSettings() {
+    extension_settings[extensionName] = extension_settings[extensionName] || {};
+    if (Object.keys(extension_settings[extensionName]).length === 0) {
+        Object.assign(extension_settings[extensionName], defaultSettings);
+    }
+    $("#hud_simu_enabled_checkbox").prop("checked", extension_settings[extensionName].enabled);
+}
+
+function onCheckboxChange(event) {
+    const value = $(event.target).prop("checked");
+    extension_settings[extensionName].enabled = value;
+    saveSettingsDebounced();
+    console.log(`[${extensionName}] Setting 'enabled' saved:`, value);
+}
 
 // Khởi tạo Extension
 jQuery(async () => {
@@ -15,6 +35,12 @@ jQuery(async () => {
        
         // Thêm vào bảng cài đặt (Cột bên phải dành cho các tiện ích giao diện)
         $("#extensions_settings2").append(settingsHtml);
+       
+        // NEW: Bind checkbox event
+        $("#hud_simu_enabled_checkbox").on("input", onCheckboxChange);
+       
+        // NEW: Load saved settings
+        loadSettings();
        
         console.log(`[${extensionName}] ✅ Loaded successfully`);
     } catch (error) {
