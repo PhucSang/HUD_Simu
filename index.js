d7ae28c3d7e01c511dd1da1a71c1e74a711b3cb2
PhucSang <nguyenphucsang5@gmail.com>
2026-04-23 16:05:48 +0700

    c

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ async function loadSettings() {
         Object.assign(extension_settings[extensionName], defaultSettings);
     }
     $("#hud_simu_enabled_checkbox").prop("checked", extension_settings[extensionName].enabled);
+    
+    updateHudVisibility();
+}
+
+function updateHudVisibility() {
+    if (extension_settings[extensionName].enabled) {
+        $("#hud-simu-overlay").show();
+    } else {
+        $("#hud-simu-overlay").hide();
+    }
 }
 
 function onCheckboxChange(event) {
     const value = $(event.target).prop("checked");
     extension_settings[extensionName].enabled = value;
     saveSettingsDebounced();
+    updateHudVisibility();
     console.log(`[${extensionName}] Setting 'enabled' saved:`, value);
 }
 
@@ -45,6 +56,20 @@ jQuery(async () => {
        
         // Thêm vào bảng cài đặt (Cột bên phải dành cho các tiện ích giao diện)
         $("#extensions_settings2").append(settingsHtml);
+       
+        // NEW: Tải và thêm HUD overlay vào body của trang
+        const hudHtml = await $.get(`${extensionFolderPath}/hud.html`);
+        $("body").append(hudHtml);
+       
+        // NEW: Logic chuyển đổi qua lại giữa các Tabs trong HUD
+        $("body").on("click", ".hud-simu-tab-btn", function() {
+            // 1. Xóa class active ở tất cả các nút và nội dung
+            $(".hud-simu-tab-btn").removeClass("active");
+            $(".hud-simu-tab-pane").removeClass("active");
+            // 2. Thêm class active cho tab được bấm
+            $(this).addClass("active");
+            $("#" + $(this).attr("data-tab")).addClass("active");
+        });
        
         // NEW: Bind checkbox event
         $("#hud_simu_enabled_checkbox").on("input", onCheckboxChange);
